c59fab2481efd0e62924f7185ce03646d6162fb2
Petikov Vladimir <vx7xv@inbox.ru>
2020-10-23 23:16:13 +0300

    Проект хранилище ссылок - начат 20.10.20

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Узелки на память</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    <div class="logo">Каталог ссылок <span>на полезные рессурсы</span></div>
+    <button class="button" type="submit">Добавить ссылку</button>
+</div>
+
+
+
+  <div class="sss">
+      <a href="https://wp-kama.ru/" target="_blank">Лучший русскоязычный сайт о WordPress</a>
+      <a href="https://developer.wordpress.org/themes/" target="_blank">Руководство Wordpress по созданию тем <span>(en)</span></a>
+      <a href="https://fonts.google.com/" target="_blank">Сайт со шрифтами от Google <span>(en)</span></a>
+      <a href="http://pixelgene.ru/" target="_blank">Pixelgene.ru - Ресурсы для веб-дизайнеров (шрифты, psd-макеты)</a>
+      <a href="https://onlinefontconverter.com/" target="_blank">Онлайн конвертер шрифтов</a>
+      
+      <br>
+      <h2>Хуки Wordpress</h2>
+      <a href="https://wp-kama.ru/id_6680/kak-rabotayut-huki.html" target="_blank">Как работают хуки в WordPress (фильтры и события)</a>
+      <a href="https://wp-kama.ru/hook/after_setup_theme" target="_blank">Хук <span>after_setup_theme</span></a>
+      <br>
+      <h2>Циклы Wordpress</h2>
+      <a href="https://wp-kama.ru/id_119/the-loop.html" target="_blank">Что такое цикл Wordpress</a>
+      <a href="https://wp-kama.ru/id_767/3-sposoba-postroeniya-tsiklov-v-wordpress.html" target="_blank">3 способа построить цикл wordpress</a>
+      <br>
+      <h2>Функции Wordpress</h2>
+      <a href="https://wp-kama.ru/function/add_action" target="_blank">Функция <span>add_action()</span></a>
+      <a href="https://wp-kama.ru/function/get_stylesheet_uri" target="_blank">Функция <span>get_stylesheet_uri()</span></a>
+      <a href="https://wp-kama.ru/function/wp_enqueue_style" target="_blank">Функция <span>wp_enqueue_style()</span></a>
+      <a href="https://wp-kama.ru/function/wp_footer" target="_blank">Функция <span>wp_footer()</span></a>
+      <a href="https://wp-kama.ru/function/wp_head" target="_blank">Функция <span>wp_head()</span></a>
+      <a href="https://wp-kama.ru/function/body_class" target="_blank">Функция <span>body_class()</span></a>
+      <a href="https://wp-kama.ru/function/bloginfo" target="_blank">Функция <span>bloginfo()</span></a>
+      <a href="https://wp-kama.ru/function/add_theme_support" target="_blank">Функция <span>add_theme_support()</span></a>
+      <a href="https://wp-kama.ru/function/get_custom_logo" target="_blank">Функция <span>get_custom_logo()</span></a>
+      <a href="https://wp-kama.ru/function/register_nav_menus" target="_blank">Функция <span>register_nav_menus()</span></a>
+      <a href="https://wp-kama.ru/function/wp_nav_menu" target="_blank">Функция <span>wp_nav_menu()</span></a>
+      <a href="https://wp-kama.ru/function/get_search_form" target="_blank">Функция <span>get_search_form()</span></a>
+      <a href="https://wp-kama.ru/function/get_posts" target="_blank">Функция <span>get_posts()</span></a>
+  </div>
+</body>
+</html>
